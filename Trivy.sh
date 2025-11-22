b3450a1558c9400c5d78357b08ab3834571076c1
Sasikumar Jada <sasikumarjada2004@gmail.com>
2025-11-22 11:42:29 +0530

    Update Trivy.sh

--- Trivy.sh
+++ Trivy.sh
@@ -1,1 +1,1 @@
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.66.0
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.67.2
